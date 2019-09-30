b3b931517ea176b29df17e13a4a31b030596a1fe
albertolaley <albertolaley@gmail.com>
2019-09-30 09:43:45 -0400

    Adding counter service

---
+++ counterservice/src/main/java/com/albertorosario/springboot_microservices/counterservice/CounterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.albertorosario.springboot_microservices.counterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CounterserviceApplication.class, args);
+  }
+
+}
