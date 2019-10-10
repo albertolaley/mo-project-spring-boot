c38144878b34085fa3d038061b69deb4a2e8346a
albertorosario <alberto_rosario@claro.com.do>
2019-10-10 12:02:40 -0400

    Change to gateway service

--- gatewayservice/src/main/java/com/albertorosario/springboot_microservices/gatewayservice/SecurityConfig.java
+++ gatewayservice/src/main/java/com/albertorosario/springboot_microservices/gatewayservice/SecurityConfig.java
@@ -1,34 +1,35 @@
 package com.albertorosario.springboot_microservices.gatewayservice;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-  @Bean
-  public UserDetailsService userDetailsService() throws Exception {
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(User.withUsername("albertolaley").password("{noop}welcome1")
-            .roles("USER").build());
-    return manager;
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//  @Bean
+//  public UserDetailsService userDetailsService() throws Exception {
+////    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//    manager.createUser(User.withUsername("albertolaley").password("{noop}welcome1")
+//            .roles("USER").build());
+//    return manager;
+//  }
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+            .csrf().disable()
+            .authorizeRequests().antMatchers("*").hasRole("USER")
+            .anyRequest().authenticated()
+            .and().httpBasic();
+    httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
-//  @Override
-//  protected void configure(HttpSecurity httpSecurity) throws Exception {
-//    httpSecurity
-//            .csrf().disable()
-//            .authorizeRequests().antMatchers("*").hasRole("USER")
-//            .anyRequest().authenticated()
-//            .and().httpBasic();
-//    httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//  }
-//
-//  @Autowired
-//  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.inMemoryAuthentication().withUser("albertolaley").password("welcome1").roles("USER");
-//  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("albertolaley").password("welcome1").roles("USER");
+  }
 }
